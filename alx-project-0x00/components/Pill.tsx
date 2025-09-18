05156da2fb99c5bcb77716a8d4f68aa67e8e54fb
sakachris <sakachris90@gmail.com>
2025-09-18 10:41:17 +0300

    rendering components

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type PillProps = {
+  title: string;
+};
+
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm">{title}</p>
+    </div>
+  );
+};
+
+export default Pill;
+
+
+// const Pill: React.FC = () => {
+//   return (
+//     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+//       <p className=" text-sm ">Title</p>
+//     </div>
+//   )
+// }
+
+// export default Pill;
