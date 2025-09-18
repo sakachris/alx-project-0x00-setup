b1308171dfb1013c4ae712490a568fb8c0a5ebf5
sakachris <sakachris90@gmail.com>
2025-09-18 10:55:08 +0300

    typing functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-
-type PillProps = {
-  title: string;
-};
+import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
@@ -13,14 +9,3 @@ const Pill: React.FC<PillProps> = ({ title }) => {
 };
 
 export default Pill;
-
-
-// const Pill: React.FC = () => {
-//   return (
-//     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-//       <p className=" text-sm ">Title</p>
-//     </div>
-//   )
-// }
-
-// export default Pill;
